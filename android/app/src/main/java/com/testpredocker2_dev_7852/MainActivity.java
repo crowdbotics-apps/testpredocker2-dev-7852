bb8a3eff3315329718510a8d2a84eebce1e47f99
Crowdbotics <team@crowdbotics.com>
2020-07-17 20:42:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/testpredocker2_dev_7852/MainActivity.java
@@ -0,0 +1,15 @@
+package com.testpredocker2_dev_7852;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "testpredocker2_dev_7852";
+    }
+}
